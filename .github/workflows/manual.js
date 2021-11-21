0fd38d40ca3715d6c81218a5449f73784e80a759
herereadthis <herereadthis@gmail.com>
2021-11-21 00:32:01 -0500

    yum

--- .github/workflows/manual.js
+++ .github/workflows/manual.js
@@ -1,1 +1,10 @@
+const chalk = require('chalk');
+const shell = require('shelljs');
+
+
+shell.config.verbose = true;
+const versionNumber = process.argv[2];
+// const childPackages = ['./meteor-db', './meteor-scripts', './meteor-server', './meteor-ui', './scripts'];
+
+console.log(`Version number is: ${versionNumber}`);
 console.log('hello can you read this line?');
