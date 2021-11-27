954e7c2880daa4d72f444adafc6cbc37cfa382e5
herereadthis <herereadthis@gmail.com>
2021-11-26 23:34:36 -0500

    base branch

--- .github/actions/bump/index.js
+++ .github/actions/bump/index.js
@@ -1,14 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const chalk = require('chalk');
 const shell = require('shelljs');
 
 shell.config.verbose = true;
 const versionNumber = core.getInput('version');
 const baseBranch = core.getInput('base_branch');
 
-console.log(chalk.green(`Version number is: ${versionNumber}`));
-console.log(chalk.green(`Base Branch is: ${baseBranch}`));
+console.log(`Version number is: ${versionNumber}`);
+console.log(`Base Branch is: ${baseBranch}`);
 shell.exec(`git checkout ${baseBranch}`);
 shell.exec(`git checkout -b bump-version-${baseBranch}`);
 shell.exec(`git add package.json package-lock.json`);
@@ -17,4 +16,4 @@ shell.exec(`git add package.json package-lock.json`);
 shell.exec(`git commit -m Bump version ${versionNumber}`);
 shell.exec(`git push origin bump-version-${baseBranch}`);
 
-console.log(chalk.green(`Pushed branch bump-version-${baseBranch}`));
+console.log(`Pushed branch bump-version-${baseBranch}`);
