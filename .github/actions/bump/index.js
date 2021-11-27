14c561d2d5a3653fb88020dc5f25211bf851fd46
herereadthis <herereadthis@gmail.com>
2021-11-27 00:23:10 -0500

    damn you sindresorhus

--- .github/actions/bump/index.js
+++ .github/actions/bump/index.js
@@ -1,5 +1,4 @@
-import core from '@actions/core';
-import github from '@actions/github';
+import * as core from '@actions/core';
 import shell from 'shelljs';
 import semverRegex from 'semver-regex';
 
