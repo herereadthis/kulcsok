f4c713976badc829d733fda8538f69a35e8d009b
herereadthis <herereadthis@gmail.com>
2021-11-26 23:46:04 -0500

    al l the typos

--- .github/actions/bump/index.js
+++ .github/actions/bump/index.js
@@ -8,12 +8,17 @@ const baseBranch = core.getInput('base_branch');
 
 console.log(`Version number is: ${versionNumber}`);
 console.log(`Base Branch is: ${baseBranch}`);
+
+if (!semverRegex().test(versionNumber)) {
+    throw new Error('Version number is not semver!');
+}
+
 shell.exec(`git checkout ${baseBranch}`);
-shell.exec(`git checkout -b bump-version-${baseBranch}`);
+shell.exec(`git checkout -b bump-version-${versionNumber}`);
 shell.exec(`git add package.json package-lock.json`);
 shell.exec(`npm version ${versionNumber} --no-git-tag-version`);
 shell.exec(`git add package.json package-lock.json`);
-shell.exec(`git commit -m Bump version ${versionNumber}`);
-shell.exec(`git push origin bump-version-${baseBranch}`);
+shell.exec(`git commit -m "Bump version ${versionNumber}"`);
+shell.exec(`git push origin bump-version-${versionNumber}`);
 
-console.log(`Pushed branch bump-version-${baseBranch}`);
+console.log(`Pushed branch bump-version-${versionNumber}`);
