c7e952c0697a52f2d369f37c4baffa71bb3cc2a5
herereadthis <herereadthis@gmail.com>
2021-11-26 23:29:42 -0500

    bump action

--- .github/actions/bump/index.js
+++ .github/actions/bump/index.js
@@ -4,9 +4,17 @@ const chalk = require('chalk');
 const shell = require('shelljs');
 
 shell.config.verbose = true;
-// const versionNumber = process.argv[2];
 const versionNumber = core.getInput('version');
-// const childPackages = ['./meteor-db', './meteor-scripts', './meteor-server', './meteor-ui', './scripts'];
+const baseBranch = core.getInput('base_branch');
 
-console.log(`Version number is: ${versionNumber}`);
-console.log('hello can you read this line?');
+console.log(chalk.green(`Version number is: ${versionNumber}`));
+console.log(chalk.green(`Base Branch is: ${baseBranch}`));
+shell.exec(`git checkout ${baseBranch}`);
+shell.exec(`git checkout -b bump-version-${baseBranch}`);
+shell.exec(`git add package.json package-lock.json`);
+shell.exec(`npm version ${versionNumber} --no-git-tag-version`);
+shell.exec(`git add package.json package-lock.json`);
+shell.exec(`git commit -m Bump version ${versionNumber}`);
+shell.exec(`git push origin bump-version-${baseBranch}`);
+
+console.log(chalk.green(`Pushed branch bump-version-${baseBranch}`));
