3b24fe28410a597aa92c6c4fb86f149c38e2ce88
herereadthis <herereadthis@gmail.com>
2021-11-28 00:29:09 -0500

    maybe

--- .github/actions/bump/index.js
+++ .github/actions/bump/index.js
@@ -9,12 +9,24 @@ const git = simpleGit();
 shell.config.verbose = true;
 const versionNumber = core.getInput('version');
 const baseBranch = core.getInput('base_branch');
+const actor = core.getInput('actor');
+const email = core.getInput('email');
 
 console.log(`Version number is: ${versionNumber}`);
 console.log(`Base Branch is: ${baseBranch}`);
+console.log(`Actor is ${actor}`);
+console.log(`Email is ${email}`);
 
 if (!semverRegex().test(versionNumber)) {
     throw new Error('Version number is not semver!');
+}
+
+try {
+    await git
+        .addConfig('user.email', getInput(email))
+        .addConfig('user.name', getInput(actor));
+} catch (err) {
+    throw err;
 }
 
 shell.exec(`git checkout ${baseBranch}`);
