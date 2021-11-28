35acbc151c60635e54941bd7bc714457e0e9cfa9
herereadthis <herereadthis@gmail.com>
2021-11-28 00:32:49 -0500

    maybe

--- .github/actions/bump/index.js
+++ .github/actions/bump/index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-// const github = require('@actions/github');
+const github = require('@actions/github');
 const shell = require('shelljs');
 const semverRegex = require('semver-regex');
 const simpleGit = require('simple-git');
@@ -9,13 +9,15 @@ const git = simpleGit();
 shell.config.verbose = true;
 const versionNumber = core.getInput('version');
 const baseBranch = core.getInput('base_branch');
-const actor = core.getInput('actor');
-const email = core.getInput('email');
+const actor = github.context.actor;
+const email = `${actor}@email.com`;
 
 console.log(`Version number is: ${versionNumber}`);
 console.log(`Base Branch is: ${baseBranch}`);
 console.log(`Actor is ${actor}`);
 console.log(`Email is ${email}`);
+console.log('github context');
+console.log(github.context);
 
 if (!semverRegex().test(versionNumber)) {
     throw new Error('Version number is not semver!');
