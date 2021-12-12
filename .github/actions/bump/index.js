5dc1e33a69053cb585950b67251556987982084e
herereadthis <herereadthis.github@gmail.com>
2021-12-12 00:38:22 -0500

    a lot of changes

--- .github/actions/bump/index.js
+++ .github/actions/bump/index.js
@@ -1,31 +1,17 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const shell = require('shelljs');
-const semverRegex = require('semver-regex');
 const simpleGit = require('simple-git');
+
+const package = require('./package.json');
 
 const git = simpleGit();
 
 shell.config.verbose = true;
 
+const version = core.getInput('version');
+
 const versionNumber = core.getInput('version');
 const baseBranch = core.getInput('base_branch');
-const repoToken = core.getInput('repo_token');
-
-const actor = github.context.actor;
-const email = `${actor}@email.com`;
-
-const newBranch = `bump-version-${versionNumber}`
-
-console.log(`Github token is: ${repoToken}`);
-console.log(`Version number is: ${versionNumber}`);
-console.log(`Base Branch is: ${baseBranch}`);
-console.log(`New Branch is: ${newBranch}`);
-console.log(`Actor is ${actor}`);
-console.log(`Email is ${email}`);
-
-// console.log('github context');
-// console.log(github.context);
 
 if (!semverRegex().test(versionNumber)) {
     throw new Error('Version number is not semver!');
@@ -35,30 +21,49 @@ if (repoToken === undefined || repoToken === null) {
     core.warning('No github_token has been detected, the action may fail if it needs to use the API');
 }
 
-const configure = async () => {
-    try {
-        await git
-            .addConfig('user.email', email)
-            .addConfig('user.name', actor)
-            .addConfig('author.email', email)
-            .addConfig('author.name', actor)
-            .addConfig('committer.email', email)
-            .addConfig('committer.name', actor)
-            .checkoutBranch(newBranch, baseBranch);
+const getNewVersion = () => {
+    if (!['major', 'minor', 'patch'].includes(version)) {
+        throw new Error('invalid version!');
+    }
 
-        shell.exec(`npm version ${versionNumber} --no-git-tag-version`);
+    const packageVersion = package.version;
+    const packageValues = packageVersion.split('.');
+
+    if (version === 'major') {
+        packageValues[0] = packageValues[0] + 1;
+    } else if (version === 'minor') {
+        packageValues[1] = packageValues[1] + 1;
+    } else {
+        packageValues[2] = packageValues[2] + 1;
+    }
+
+    return packageValues.join('.');
+};
+
+const updatePackages = async (newBranch) => {
+    try {
+        await git.checkoutBranch(newBranch, baseBranch);
+
+        shell.exec(`npm version ${version} --no-git-tag-version`);
 
         await git
             .add('package.json')
-            .add('package-lock.json')
-            .commit(`Bump version ${versionNumber}`)
-            .push('origin', newBranch);
+            .add('package-lock.json');
 
-        console.log(`Pushed branch ${newBranch}`);
-        core.setOutput("newBranch", newBranch);
     } catch (err) {
         throw err;
     }
 };
 
-configure();
+const newVersionNumber = getNewVersion();
+const newBranch = `bump-version-${newVersionNumber}`;
+
+console.log('***');
+console.log(`New Vervsion is: <${newVersionNumber}>`);
+console.log(`New Branch is: <${newBranch}>`);
+console.log('***');
+
+core.setOutput("new_version_number", newVersionNumber);
+core.setOutput("new_branch", newBranch);
+
+updatePackages(newBranch);
