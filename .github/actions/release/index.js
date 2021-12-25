a2e92e97f2f9a4282dc230ae3b35ff5ea763bd9c
herereadthis <herereadthis.github@gmail.com>
2021-12-24 23:24:35 -0500

    warnings

--- .github/actions/release/index.js
+++ .github/actions/release/index.js
@@ -22,35 +22,46 @@ const getCurrentVersion = () => {
 
 
 const run = async () => {
+    core.warning(1);
     try {
         // Get authenticated GitHub client (Ocktokit): 
         // https://github.com/actions/toolkit/tree/master/packages/github#usage
         const github = new GitHub(process.env.GITHUB_TOKEN);
 
+        core.warning(2);
         await git
             .addConfig('user.email', 'github-actions')
             .addConfig('user.name', 'github-actions@github.com');
 
+        core.warning(3);
         const {
             owner: currentOwner,
             repo: currentRepo
         } = context;
 
+
+        core.warning(4);
+
         const version = getCurrentVersion();
         const tag = `v${version}`;
         const prerelease = CREATE_PROD_BUILD === false;
+        core.warning(5);
 
+        core.warning('can you see me?');
         core.warning(version);
         core.warning(prerelease);
         core.warning(tag);
         core.warning(currentOwner);
         core.warning(currentRepo);
 
-
+        core.warning(7);
+        
+        
         core.setOutput("version", version);
         core.setOutput("tag", tag);
         core.setOutput("owner", currentOwner);
         core.setOutput("repo", currentRepo);
+        core.warning(7);
     } catch (err) {
         core.setFailed(err.message);
     }
