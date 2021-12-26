31113509cf4a7e2172fd1f4e4021839807c58542
herereadthis <herereadthis.github@gmail.com>
2021-12-26 00:38:52 -0500

    simplify

--- .github/actions/release/script.js
+++ .github/actions/release/script.js
@@ -1,12 +1,18 @@
-module.exports = async ({github, context, core}) => {
+const run = async ({github, context, core}) => {
 
-    core.info('hello world');
-    
-    const {SHA} = process.env
-    const commit = await github.rest.repos.getCommit({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      ref: `${SHA}`
-    })
-    core.exportVariable('author', commit.data.commit.author.email)
-  }
+  core.info('hello world');
+
+  // const {SHA} = process.env
+  // const commit = await github.rest.repos.getCommit({
+  //   owner: context.repo.owner,
+  //   repo: context.repo.repo,
+  //   ref: `${SHA}`
+  // })
+
+  core.info(context);
+
+  // core.exportVariable('author', commit.data.commit.author.email)
+};
+
+
+module.exports = run;
