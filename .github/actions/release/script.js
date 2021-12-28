d4f83e7d557dbcc74600a65b50970df7e0edc7b2
herereadthis <herereadthis.github@gmail.com>
2021-12-27 23:45:12 -0500

    list commits

--- .github/actions/release/script.js
+++ .github/actions/release/script.js
@@ -1,24 +1,36 @@
 const run = async ({github, context, core}) => {
 
-  core.info('hello world');
+    core.info('hello world');
 
-  // const {SHA} = process.env
-  // const commit = await github.rest.repos.getCommit({
-  //   owner: context.repo.owner,
-  //   repo: context.repo.repo,
-  //   ref: `${SHA}`
-  // })
+    // const {SHA} = process.env
+    // const commit = await github.rest.repos.getCommit({
+    //   owner: context.repo.owner,
+    //   repo: context.repo.repo,
+    //   ref: `${SHA}`
+    // })
 
-  core.info('github')
-  core.info(JSON.stringify(github))
-  core.info('context');
-  core.info(JSON.stringify(context));
-  core.info('repo');
-  core.info(JSON.stringify(context.repo));
-  core.info('repos');
-  core.info(JSON.stringify(context.repos));
+    const {
+        owner,
+        repo
+    } = context.repo;
 
-  // core.exportVariable('author', commit.data.commit.author.email)
+    core.info('process.env')
+    core.info(JSON.stringify(process.env))
+    core.info('github')
+    core.info(JSON.stringify(github))
+    core.info('context');
+    core.info(JSON.stringify(context));
+    core.info('repo');
+    core.info(JSON.stringify(context.repo));
+    core.info('repos');
+    core.info(JSON.stringify(context.repos));
+
+    const commits = await github.rest.repos.listCommits({owner, repo});
+
+    core.info('commits');
+    core.info(JSON.stringify(commits));
+
+    // core.exportVariable('author', commit.data.commit.author.email)
 };
 
 
