78d51af0b5ff55b0faba3bfa30a5950e65c5471a
herereadthis <herereadthis.github@gmail.com>
2021-12-29 00:59:17 -0500

    last one

--- .github/actions/release/script.js
+++ .github/actions/release/script.js
@@ -1,68 +1,81 @@
 const exec = require('@actions/exec');
 const shell = require('shelljs');
 
+const getBody = (sha, url, message, branch) => {
+    return `
+SHA: ${sha}
+URL: [${message}](${url})
+    `;
+}
+
 const run = async ({github, context, core}) => {
-
     core.info('hello world');
+    try {
+        await exec.exec('ls');
 
-    await exec.exec('ls');
+        const currentVersion = shell.exec(`echo $(node -p -e "require('./package.json').version")`);
 
+        // const {SHA} = process.env
+        // const commit = await github.rest.repos.getCommit({
+        //   owner: context.repo.owner,
+        //   repo: context.repo.repo,
+        //   ref: `${SHA}`
+        // })
 
-    const currentVersion = shell.exec(`echo $(node -p -e "require('./package.json').version")`);
+        const {
+            owner,
+            repo
+        } = context.repo;
 
-    // const {SHA} = process.env
-    // const commit = await github.rest.repos.getCommit({
-    //   owner: context.repo.owner,
-    //   repo: context.repo.repo,
-    //   ref: `${SHA}`
-    // })
+        const {
+            branch,
+            create_prod_build: createProductionBuild
+        } = process.env;
 
-    const {
-        owner,
-        repo
-    } = context.repo;
+        core.info('currentVersion')
+        core.info(currentVersion)
+        core.info('createProductionBuild')
+        core.info(createProductionBuild)
+        core.info('github')
+        core.info(JSON.stringify(github))
+        core.info('context');
+        core.info(JSON.stringify(context));
+        core.info('repo');
+        core.info(JSON.stringify(context.repo));
+        core.info('repos');
+        core.info(JSON.stringify(context.repos));
 
+        const commits = await github.rest.repos.listCommits({
+            owner,
+            repo,
+            per_page: 1
+        });
 
+        const {
+            html_url,
+            sha,
+            commit
+        } = commits.data[0];
 
-    core.info('currentVersion')
-    core.info(currentVersion)
-    core.info('process.env')
-    core.info(JSON.stringify(process.env))
-    core.info('github')
-    core.info(JSON.stringify(github))
-    core.info('context');
-    core.info(JSON.stringify(context));
-    core.info('repo');
-    core.info(JSON.stringify(context.repo));
-    core.info('repos');
-    core.info(JSON.stringify(context.repos));
+        core.info('sha');
+        core.info(sha);
+        core.info('html_url');
+        core.info(html_url);
+        core.info('commit.message');
+        core.info(commit.message);
 
-    const commits = await github.rest.repos.listCommits({
-        owner,
-        repo,
-        per_page: 1
-    });
-
-    const {
-        html_url,
-        sha,
-        commit
-    } = commits.data[0];
-
-    core.info('sha');
-    core.info(sha);
-    core.info('html_url');
-    core.info(html_url);
-    core.info('commit.message');
-    core.info(commit.message);
-
-    // octokit.rest.repos.createRelease({
-    //     owner,
-    //     repo,
-    //     tag_name,
-    //   });
-
-    // core.exportVariable('author', commit.data.commit.author.email)
+        await github.rest.repos.listCommits({
+            owner,
+            repo,
+            target_commitish: sha,
+            name: createProductionBuild ? `${currentVersion} Production` : `${currentVersion} Staging`,
+            body: getBody(sha, html_url, commit.mesage, branch),
+            prerelease: !createProductionBuild
+        });
+    } catch (err) {
+        core.setFailed(err.message);
+        throw err;
+    }
 };
 
 
